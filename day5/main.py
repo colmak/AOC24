b1196cbe3759830dec446b6390d977ccf64fb9d4
colmak <69098858+colmak@users.noreply.github.com>
2024-12-04 08:45:20 -0500

    day 5 setup

---
+++ day5/main.py
@@ -0,0 +1,24 @@
+import os
+def read_file(fname):
+    if not os.path.exists(fname):
+        raise Exception(f"File {fname} not found!")
+
+    with open(fname, 'r') as f:
+        lines = [l.strip() for l in f.readlines()]
+        return lines
+
+def part1(lines):
+    print(lines)
+    pass
+
+
+def part2(lines):
+    pass
+
+def main():
+    lines = read_file("day5/input.txt")
+    part1(lines)
+    # part2(lines)
+
+if __name__ == "__main__":
+    main()
