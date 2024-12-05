c3e4c7f61ab53bfd35e58a396b4479d18b004d38
colmak <69098858+colmak@users.noreply.github.com>
2024-12-05 17:23:07 -0500

    day 5

--- day5/main.py
+++ day5/main.py
@@ -1,24 +1,62 @@
 import os
-def read_file(fname):
-    if not os.path.exists(fname):
-        raise Exception(f"File {fname} not found!")
 
-    with open(fname, 'r') as f:
-        lines = [l.strip() for l in f.readlines()]
-        return lines
+def read_file(file):
+    with open(file, "r") as f:
+        raw_rules, raw_pages = f.read().split("\n\n")
+        rules = [rule.split("|") for rule in raw_rules.split("\n")]
+        pages = [line.split(",") for line in raw_pages.split("\n")]
+        return rules, pages
 
-def part1(lines):
-    print(lines)
-    pass
+def part1(file):
+    rules, pages = read_file(file)
+    result = []
+    for line in pages:
+        right_order = True
+        for rule in rules:
+            try:
+                num1 = line.index(rule[0])
+                num2 = line.index(rule[1])
+            except ValueError:
+                continue
+            if num1 > num2:
+                right_order = False
+                break
+        if right_order:
+            result.append(int(line[(len(line) - 1) // 2]))
+    print(sum(result))
 
+def part2(file):
+    rules, pages = read_file(file)
+    result = []
 
-def part2(lines):
-    pass
+    def check_line(line, rules):
+        for rule in rules:
+            try:
+                num1 = line.index(rule[0])
+                num2 = line.index(rule[1])
+            except ValueError:
+                continue
+            if num1 > num2:
+                global right_order
+                right_order = False
+
+                line[num1], line[num2] = rule[1], rule[0]
+                check_line(line, rules)
+        return line
+
+    for line in pages:
+        global right_order
+        right_order = True
+        line = check_line(line, rules)
+
+        if not right_order:
+            result.append(int(line[(len(line) - 1) // 2]))
+    print(sum(result))
 
 def main():
-    lines = read_file("day5/input.txt")
-    part1(lines)
-    # part2(lines)
+    lines = "day5/input.txt"
+    # part1(lines)
+    part2(lines)
 
 if __name__ == "__main__":
     main()
